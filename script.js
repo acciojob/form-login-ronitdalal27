d08efaa4aada9198992ffafcfcecf4a3902ecf11
ronitdalal27 <dalalronit131@gmail.com>
2025-01-13 20:19:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function getFormvalue() {
     //Write your code here
-	const fName = document.getElementById("fname");
-	const lName = document.getElementById("lname");
-	alert(fname+" "+lName);
+	const inps = document.querySelectorAll("input");
+	let fname = inps[0].value;
+	let lname = inps[1].value;
+	alert(fname+" "+lname);
+	
 
 }
