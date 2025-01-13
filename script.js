2a6d0f73d21957849e40907aa2940e7a0bc5a663
ronitdalal27 <dalalronit131@gmail.com>
2025-01-13 20:14:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const fName = document.getElementById("fname");
+	const lName = document.getElementById("lname");
+	alert(fname+" "+lName);
 
 }
